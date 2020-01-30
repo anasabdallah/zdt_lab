127a7b610f7796e42929795097c677398c0b6b56
Anas Abdullah <anas.abdullah@optimumpartners.co>
2020-01-30 10:53:16 -0500

    fix-path

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Creating Network Bridge .."
+docker network create traefik_webgateway
+
 echo "Starting the load balancer .."
 docker-compose --project-name=traefik -f docker-compose.traefik.yml up -d
